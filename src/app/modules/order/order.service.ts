107a7a585588813cb2792c12aafe984d6705e14d
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-05-23 18:48:43 +0600

    order service

---
+++ src/app/modules/order/order.service.ts
@@ -0,0 +1,56 @@
+import { ProductModel } from "../product/product.model";
+import { Order } from "./order.interface";
+import { OrderModel } from "./order.model";
+
+const createOrder = async (order: Order) => {
+  const { productId: _id, quantity } = order;
+
+  // checking product existence
+  const product = await ProductModel.findOne({ _id });
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  // checking the quantity
+  if (product?.inventory?.quantity < quantity) {
+    throw new Error("Insufficient quantity available in inventory");
+  }
+
+  const updatedQuantity = product?.inventory?.quantity - quantity;
+  const inStock = updatedQuantity > 0;
+  const updateProductData = {
+    $set: {
+      "inventory.quantity": updatedQuantity,
+      "inventory.inStock": inStock,
+    },
+  };
+
+  const updatedProduct = await ProductModel.findByIdAndUpdate(
+    { _id },
+    updateProductData
+  );
+
+  if (!updatedProduct) {
+    throw new Error("Something went wrong while updating");
+  }
+
+  // Create order
+  const result = await OrderModel.create(order);
+  return result;
+};
+
+// get all the orders from db
+const getAllOrder = async (queryParams: any) => {
+  let query: object = {};
+
+  if (queryParams?.email) {
+    query = { email: queryParams?.email };
+  }
+  const result = await OrderModel.find(query);
+  return result;
+};
+
+export const OrderServices = {
+  createOrder,
+  getAllOrder,
+};
